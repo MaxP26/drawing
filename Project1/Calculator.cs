c5572ca88aa4d1e3d6672746fd3ec0c256da87a5
MaxP26 <penkovm6@gmail.com>
2023-12-31 01:55:56 +0200

    first version

---
+++ Project1/Calculator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal static class Calculator
+    {
+        const string dgt = @"[0-9]";
+        const string usnum = $"({dgt}+[.,]?{dgt}*)";
+        const string snum = $"(\\(-{usnum}\\)|-?{usnum})";
+        const string prp = $"(\\(-{usnum}?x\\)|-?{usnum}?x)";
+        const string opd = $"({snum}|{prp})";
+        const string opr = $" ?[+*/\\-^] ?";
+        const string formulaPattern = $"^{opd}({opr}{opd})*$";
+        static Regex reg = new Regex(formulaPattern, RegexOptions.Compiled);
+        static Dictionary<int, string> opPriority = new()
+        {
+            {0,"^" },
+            {1,"*/"},
+            {2,"+~"},
+        };
+        static Dictionary<char, Func<Expression, Expression, Expression>> operations = new()
+        {
+            {'^',Expression.Power },
+            {'*',Expression.Multiply },
+            {'/',Expression.Divide },
+            {'+',Expression.Add },
+            {'~',Expression.Subtract },
+        };
+        public static bool isValid(string form)
+        {
+            return reg.IsMatch(form);
+        }
+        static ParameterExpression X { get; } = Expression.Parameter(typeof(double), "x");
+        public static T GetDelegate<T>(string func) where T : Delegate
+        {
+            return Expression.Lambda<T>(GetExpression(func), new ParameterExpression[] { X }).Compile();
+        }
+        static Expression GetExpression(string func)
+        {
+            func = func.Replace(" ", "");
+            func = func.Replace("(", "");
+            func = func.Replace(")", "");
+            func = func.Replace(".", ",");
+            func = Regex.Replace(func, @"([^^\-+/*])-",@"$1~");
+            if (Regex.IsMatch(func, $"^{opd}$"))
+            {
+                if (Regex.IsMatch(func, $"^{snum}$"))
+                    return Expression.Constant(double.Parse(func));
+                else
+                {
+                    if (Regex.IsMatch(func, @"^-?x$") )func=func.Replace("x", "1");
+                    else func = func.Replace("x", "");
+                    return Expression.Multiply(
+                    Expression.Constant(double.Parse(func)),X);
+                }
+            }
+            else
+            {
+                foreach (var opp in opPriority.Reverse())
+                {
+                    foreach (var c in opp.Value)
+                    {
+                        if (func.Contains(c))
+                            return func.Split(c)
+                                .Select(func => GetExpression(func))
+                                .Aggregate(Expression.Empty() as Expression,
+                                (a, b) =>
+                                {
+                                    if (a.GetType() == typeof(DefaultExpression) &&
+                                        (a as DefaultExpression).Type == typeof(void))
+                                        return b;
+                                    else
+                                        return operations[c](a, b);
+                                });
+                    }
+                }
+            }
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ Project1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PaintButton.Click += PaintButton_Click;
+        }
+        private void PaintButton_Click(object sender, RoutedEventArgs e)
+        {
+            FormValidationLabel.Visibility = Visibility.Collapsed;
+            FormTextBox.BorderBrush = new SolidColorBrush(Colors.Black);
+            TrianglePolygon.Visibility = TriangleRadioButton.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            RectanglePolygon.Visibility = RectangleRadioButton.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            CircleElipse.Visibility = CircleRadioButton.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            EllipseElipse.Visibility = ElipsRadioButton.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            TextBorder.Visibility = TextRadioButton.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            FuncImage.Visibility = FormRadioButton.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            if (TextRadioButton.IsChecked == true)
+            {
+                ShowTextBlock.Text = TextTextBox.Text;
+            }
+            if (FormRadioButton.IsChecked == true)
+            {
+                if (!Calculator.isValid(FormTextBox.Text))
+                {
+                    FormValidationLabel.Visibility = Visibility.Visible;
+                    FormTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                    FormTextBox.Focus();
+                    return;
+                }
+                var calc = Calculator.GetDelegate<Func<double, double>>(FormTextBox.Text);
+                double w = DrawAreaCanvas.ActualWidth, h = DrawAreaCanvas.ActualHeight;
+                GeometryGroup geometryGroup = new GeometryGroup(), xy = new();
+                xy.Children.Add(new LineGeometry(new Point(-w / 2, 0), new Point(w / 2, 0)));
+                xy.Children.Add(new LineGeometry(new Point(0, -h / 2), new Point(0, h / 2)));
+                double y;
+                for (double x = -w / 2; x < w / 2; x += 0.1)
+                {
+                    y = calc(x/10)*10;
+                    if (y > -h / 2 && y < h / 2)
+                        geometryGroup.Children.Add(new RectangleGeometry(new Rect(x, y, 1, 1)));
+                }
+                GeometryDrawing geometryDrawing = new GeometryDrawing(),xydr=new();
+                xydr.Geometry = xy;
+                xydr.Pen = new Pen(new SolidColorBrush(Colors.Gray), 2);
+                xydr.Pen.DashStyle=DashStyles.Dash;
+                geometryDrawing.Geometry = geometryGroup;
+                geometryDrawing.Brush = new SolidColorBrush(Colors.Black);
+                geometryDrawing.Pen = new Pen(new SolidColorBrush(Colors.Black), 1);
+                DrawingGroup draw = new DrawingGroup();
+                draw.Children.Add(geometryDrawing);
+                draw.Children.Add(xydr);
+                DrawingImage drawingImage=new DrawingImage(draw);
+                drawingImage.Freeze();
+                FuncImage.Source = drawingImage;
+            }
+        }
+    }
+}
